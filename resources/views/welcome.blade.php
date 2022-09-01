d3a32a156d0f74e2d891b8a0e1d19adcdc3a6a14
Moaaz-Rafique <moaazrafiquetk@gmail.com>
2022-09-01 20:45:32 +0500

    update main heading

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -407,7 +407,7 @@
 <body class="antialiased">
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         <div>
-            <h1>My Users</h1>
+            <h1>The List</h1>
 
             @foreach ($listItems as $listItem)
             <form method="post" action="{{ route('markComplete', $listItem->id) }}" accept-charset="UTF-8">
